f877e70c94d173f20af9bb7f0123c76523cf4ce6
Kelvin Mwirigi <kmwirigi05@gmail.com>
2025-03-24 07:52:40 +0300

    adds index.js

--- index.js
+++ index.js
@@ -9,4 +9,14 @@ function fuzzyMatch(drivers, letters) {
 
 function matchName(drivers, name) {
     return drivers.filter(driver => driver.name === name);
+}function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
 }
